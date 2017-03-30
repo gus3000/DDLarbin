3ad4d32ab978e862a49c94490b1e62dddd6fada2
gus3000 <gus3000spam@gmail.com>
2017-03-30 17:08:15 +0200

    character JSON parsing

--- controller.js
+++ controller.js
@@ -1,6 +1,33 @@
 // Controller
+var fs = require('fs')
+
+
+exports.Characters = function () {
+  dirname = './characters/';
+  chars = [];
+  filenames = fs.readdirSync(dirname);
+
+  filenames.forEach(function (filename) {
+    c = require(dirname + filename);
+    chars.push(c);
+  })
+  return chars;
+}
+
+exports.Character = function (name) {
+  try {
+    return require('./characters/' + name);
+  }
+  catch (e) {
+    return '';
+  }
+}
 
 exports.printCharacter = function (name) {
-  char = require("./characters/" + name); // this is genius
-  console.log(char);
+  try {
+    char = require("./characters/" + name); // this is genius
+    console.log(char);
+  } catch (e) {
+    console.log('{}')
+  }
 }
