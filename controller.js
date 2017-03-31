88ee3b298fbd115eac89ae34383b1ddd247e84a3
gus3000 <gus3000spam@gmail.com>
2017-03-31 15:07:34 +0200

    proficiency handling

--- controller.js
+++ controller.js
@@ -30,12 +30,35 @@ exports.Character = function (name, complete) {
 }
 
 exports.CompleteCharacter = function (char) {
+
+  //proficiency
+  totalLevels = 0;
+  classes = Object.keys(char.header.classes);
+
+  classes.forEach(className => {
+    totalLevels += char.header.classes[className];
+  });
+  char.proficiency = Math.floor(((totalLevels) - 1) / 4) + 2;
+  char.proficiency = char.proficiency >= 0 ? "+" + char.proficiency : char.proficiency;
+
+
+  //ability scores
   char.abilityScores = {};
   Object.keys(char.abilities).forEach(a => {
     val = char.abilities[a];
     modVal = Math.floor((val - 10) / 2);
     char.abilityScores[a] = modVal >= 0 ? "+" + modVal : modVal;
-  })
+  });
+
+  //saving throws
+  char.savingThrows = {};
+  Object.keys(char.abilityScores).forEach(a => {
+    score = parseInt(char.abilityScores[a]);
+    if (char.proficiencies[a]) {
+      score += parseInt(char.proficiency);
+    }
+    char.savingThrows[a] = score >= 0 ? "+" + score : score;
+  });
   return char;
 }
 
@@ -56,36 +79,49 @@ exports.ComputeSheet = function (name, baseSheet) {
   sheetName = sheetsPath + name + '.svg';
 
   character = exports.Character(name, true);
+  console.log('computing sheet for ' + name + ' : ' + JSON.stringify(character, null, 4));
   aliases = require('./aliases');
 
   svgContent = fs.readFileSync(baseSheetName).toString();
   svgNewContent = svgContent.replace(/\$\$(.*)\$\$/g, (match, fieldName, offset, string) => {
     //console.log('found ' + fieldName + 'from '+ name);
     fields = fieldName.split('.');
-    current = character;
-    for (var i = 0; i < fields.length; i++) {
-      potentialAlias = aliases[fields[i]];
-      if (potentialAlias) {
-        fields[i] = potentialAlias;
+    if (character.fields[0] != undefined) {
+      current = character;
+      for (var i = 0; i < fields.length; i++) {
+        potentialAlias = aliases[fields[i]];
+        if (potentialAlias) {
+          fields[i] = potentialAlias;
+        }
+        // console.log('before : ' + current);
+        current = current[fields[i]];
+        // console.log('after ' + fields[i] + ': ' + current);
       }
-      // console.log('before : '+ current);
-      current = current[fields[i]];
-      // console.log('after '+ fields[i] +': '+ current);
-    }
-    if (typeof current === 'object') {
-      s = "";
-      keys = Object.keys(current);
-      if(keys.length > 1)
-      {
-        console.log("complex object to print ! This could go wrong.")
+      if (typeof current === 'object') {
+        s = "";
+        keys = Object.keys(current);
+        if (keys.length > 1) {
+          //console.log("complex object to print ! This could go wrong.")
+        }
+        keys.forEach(v => {
+          s += v + ": " + current[v] + "\n";
+        });
+        return s;
       }
-      keys.forEach(v => {
-        s += v + ": "+ current[v] + "\n";
-      });
-      return s;
-    }
 
-    return current;
+      return current;
+    }
+    else // it's a keyword
+    {
+      field = fields[0];
+      potentialAlias = aliases[field];
+        if (potentialAlias) {
+          field = potentialAlias;
+        }
+      
+        //TODO
+
+    }
   });
 
   fs.writeFileSync(sheetName, svgNewContent);
