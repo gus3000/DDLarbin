ce0a3efd1663613034605ea38f9abe14cde818dc
gus3000 <gus3000spam@gmail.com>
2017-04-10 17:27:51 +0200

    new assert

--- test/all.js
+++ test/all.js
@@ -1,4 +1,7 @@
-// sample test to please Travis-ci
+exports['testing random asserts'] = function(assert) {
+    assert.ifError(0);
+}
+
 exports['test that logs all failures'] = function(assert) {
   //assert.equal(2 + 2, 5, 'assert failure is logged')
   assert.equal(3 + 2, 5, 'assert pass is logged')
