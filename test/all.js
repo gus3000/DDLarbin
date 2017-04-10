79740c02042cbd12ce8bb6a98bd01281c0304364
gus3000 <gus3000spam@gmail.com>
2017-04-10 13:04:55 +0200

    sample test

---
+++ test/all.js
@@ -0,0 +1,7 @@
+// sample test to please Travis-ci
+exports['test that logs all failures'] = function(assert) {
+  //assert.equal(2 + 2, 5, 'assert failure is logged')
+  assert.equal(3 + 2, 5, 'assert pass is logged')
+}
+ 
+if (module == require.main) require('test').run(exports)
