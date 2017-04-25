78b9f25fb438b9526abbfcab6c00cfbd42883d9d
gus3000 <gus3000spam@gmail.com>
2017-04-25 15:28:49 +0200

    basic tests

--- test/all.js
+++ test/all.js
@@ -1,10 +1,53 @@
 var assert = require('assert');
-var parser = require('../parser');
+var fs = require('fs-extra');
+var controller = require('../controller');
 
-describe('Query', function() {
-  describe('parsing', function() {
-    it('simple query', function() {
-      
-    })
+var oldDir;
+var tmpDir = '/tmp/ddlarbin/';
+
+before(function () {
+  oldDir = process.cwd()
+  fs.mkdirpSync(tmpDir);
+  process.chdir(tmpDir);
+  fs.mkdirSync('characters');
+  fs.mkdirSync('sheets');
+});
+
+after(function () {
+  fs.removeSync(tmpDir);
+  console.log('old directory : ' + oldDir);
+  process.chdir(oldDir);
+});
+
+describe('Controller', function () {
+  describe('#Characters', function () {
+    it('without characters', function () {
+      assert.deepEqual(controller.Characters(), []);
+    });
+    it('adding character', function () {
+      dummy = {
+        "name": "dummy",
+        "header": {
+          "classes": {},
+          "background": "",
+          "playerName": "nobody",
+          "race": "",
+          "alignment": "",
+          "xp": 0
+        },
+        "abilities": {
+          "strength": 0,
+          "dexterity": 0,
+          "constitution": 0,
+          "intelligence": 0,
+          "wisdom": 0,
+          "charisma": 0
+        },
+        "inspiration": false
+      };
+      controller.AddCharacter(dummy);
+      assert.deepEqual(controller.Characters(), []);
+      assert.deepEqual(controller.Character(dummy.name), dummy);
+    });
   });
 });
