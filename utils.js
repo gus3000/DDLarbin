4a5ccf910047b659963d7eaabf27d3e9976a195b
gus3000 <gus3000spam@gmail.com>
2017-04-28 14:14:33 +0200

    moved utils.js to root

--- utils.js
+++ utils.js
@@ -10,14 +10,36 @@ exports.includedRecursively = function (o1, o2) {
             if (o2[key] == null) {
                 allequal = false;
             }
-            allequal = allequal && exports.includedRecursively(o1[key],o2[key]);
-        })
+            allequal = allequal && exports.includedRecursively(o1[key], o2[key]);
+        });
 
         return allequal;
     }
     if (o1 === o2) { return true; }
     return false;
 }
+
+exports.handleDefaultValues = function (char, def) {
+    if (typeof char !== 'object') {
+        return char;
+    }
+    //console.log('char before :' + JSON.stringify(char));
+    Object.keys(def).forEach(function (key, index) {
+        if (key in char) {
+            char[key] = exports.handleDefaultValues(char[key], def[key]);
+        }
+        else {
+            char[key] = def[key];
+        }
+    });
+    //console.log('char after :' + JSON.stringify(char));
+    return char;
+}
+
+// console.log(JSON.stringify(exports.handleDefaultValues({ 'a': 3 }, { 'a': 2, 'b': 5 })));
+// console.log(JSON.stringify(exports.handleDefaultValues({ 'a': { 'c': 7 } }, { 'a': 2, 'b': 5 })));
+// console.log(JSON.stringify(exports.handleDefaultValues({ 'a': { 'c': 7, 'd': 9 } }, { 'a': {'e': 12}, 'b': 5 })));
+
 
 // console.log(exports.includedRecursively(3, 3));
 // console.log(exports.includedRecursively(3, 4));
